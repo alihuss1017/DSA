cc5891b317a403e067e27610733cf2001ba5615e
Ali <alihuss1017@gmail.com>
2023-04-11 01:23:10 -0700

    [ADD] activity 1 for arrays.py

--- Notes/arrays.py
+++ Notes/arrays.py
@@ -2,3 +2,22 @@ expenses = [2200, 2350, 2600, 2130, 2190]
 
 # In Feb, how many dollars you spent extra compare to January?
 print(expenses[1] - expenses[0])
+
+#Total expense in first three months
+print(expenses[0] + expenses[1] + expenses[2])
+
+#Find out if you spent exaactly 2000 dollars in any month
+
+for n in expenses:
+    if n == 2000:
+        print("True")
+    else:
+        print("False")
+
+#June just finished and expense is $1980. Add it to the list.
+expenses.append(1980)
+print(expenses)
+
+#You returned an item that you bought in a month of April and got a refund of $200. Make a correction to the expense list.
+expenses[3] -= 200
+print(expenses)
